22e31c6bd0d0686d41b512601286bd9792e12a04
LeKazo <70152390+LeKazo@users.noreply.github.com>
2022-04-07 08:27:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import cv2
-import numpy as np
+import cv2 
 
-cap = cv2.VideoCapture(0)
+eye_cascade = cv2.CascadeClassifier('eye.xml')
+def detect(gray ,frame):
+    eyes =eye_cascade.detectMultiScale(gray, 1.1, 3)
+    for (x,y ,w ,h) in eyes :
+        cv2.rectangle(frame , (x,y), (x+w ,y+h) , (255,0,0) ,2 )
+        roi_gray = gray[ y:y+h ,x:x+w ]
+        roi_color = frame[y:y+h ,x:x+w ]     
+    return frame 
 
-while True:
-    _, frame = cap.read()
-
-    cv2.imshow("Frame", frame)
-
-    key = cv2.waitKey(1)
-    if key == 27 == ord('a'):
-        break
-
-cap.release()
+capture = cv2.VideoCapture(0)
+while True :
+     _, frame =capture.read()
+     gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
+     canvas =detect(gray,frame) 
+     cv2.imshow('Eye',canvas) 
+     if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+capture.release()
 cv2.destroyAllWindows()
